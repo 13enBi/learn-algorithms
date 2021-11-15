7e734c8fc9567fc58caa65db7b075b92ccb1b01b
13enBi <2210576534@qq.com>
2021-11-15 21:38:36 +0800

    dev

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -7,7 +7,20 @@ export const wrapMicrotask =
 	(...args: T) =>
 		queueMicrotask(() => callback(...args));
 
-export const take =
-	<T extends any[], R>(fn: Func<T, R>, count: number) =>
-	(...args: T): R | undefined =>
-		count-- ? fn(...args) : void 0;
+export const take = <T extends any[], R>(fn: Func<T, R>, count: number) =>
+	count <= 0
+		? fn
+		: (...args: T): R => {
+				let lastResult: R = fn(...args);
+				return count-- > 1 ? (lastResult = fn(...args)) : lastResult;
+		  };
+
+export const callWithErrorHandler =
+	<T extends any[], R>(fn: Func<T, R>, handler: Func<[any], any>) =>
+	(...args: T): R | void => {
+		try {
+			return fn(...args);
+		} catch (error) {
+			handler(error);
+		}
+	};
